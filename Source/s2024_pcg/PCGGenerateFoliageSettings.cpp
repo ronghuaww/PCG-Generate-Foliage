f92cf0d708cc7d2be9d8a4112a8fc2a0e5e63b1b
ronghuaww <wg.ronghua@gmail.com>
2024-10-23 00:00:37 -0500

    pcg point set up

--- Source/s2024_pcg/PCGGenerateFoliageSettings.cpp
+++ Source/s2024_pcg/PCGGenerateFoliageSettings.cpp
@@ -48,6 +48,39 @@ bool FPCGGenerateFoliageElement::ExecuteInternal(FPCGContext *Context) const
     const double Decay = Settings->Decay;
     const bool RelativeScaling = Settings->RelativeScaling;
 
+	for (const FPCGTaggedData& Input : Inputs)
+	{
+		TRACE_CPUPROFILER_EVENT_SCOPE(FPCGTransformPointsElement::Execute::InputLoop);
+		FPCGTaggedData& Output = Outputs.Add_GetRef(Input);
+
+		const UPCGSpatialData* SpatialData = Cast<UPCGSpatialData>(Input.Data);
+
+		if (!SpatialData)
+		{
+			PCGE_LOG(Error, GraphAndLog, LOCTEXT("InputMissingSpatialData", "Unable to get Spatial data from input"));
+			continue;
+		}
+
+		const UPCGPointData* PointData = SpatialData->ToPointData(Context);
+
+		if (!PointData)
+		{
+			PCGE_LOG(Error, GraphAndLog, LOCTEXT("InputMissingPointData", "Unable to get Point data from input"));
+			continue;
+		}
+
+		const TArray<FPCGPoint>& Points = PointData->GetPoints();
+		
+		UPCGPointData* OutputData = NewObject<UPCGPointData>();
+		OutputData->InitializeFromData(PointData);
+		TArray<FPCGPoint>& OutputPoints = OutputData->GetMutablePoints();
+		Output.Data = OutputData;
+
+		// FPCGAsync::AsyncPointProcessing(Context, Points.Num(), OutputPoints, [&](int32 Index, FPCGPoint& OutPoint)
+		// {
+
+		// }
+    }
     return true;
 }
 
